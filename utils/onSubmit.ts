f2065f5b728b4807e60e88cc6f733c6d00cd4a01
dudleyneedham <dudleyneedham1993@gmail.com>
2024-08-14 14:52:36 +0200

    refactor: updating the onsubmit to split the function into smaller bites

--- utils/onSubmit.ts
+++ utils/onSubmit.ts
@@ -102,189 +102,39 @@ export function onSubmit({
 
       setBusyMessage("Validating...");
 
-      let messageId: string;
       let transfer: Transfer;
       switch (source.type) {
         case "substrate": {
-          if (destination.type !== "ethereum")
-            throw Error(`Invalid form state: destination type mismatch.`);
-          if (source.paraInfo === undefined)
-            throw Error(
-              `Invalid form state: source does not have parachain info.`,
-            );
-          if (polkadotAccount === null)
-            throw Error(`Polkadot Wallet not connected.`);
-          if (polkadotAccount.address !== data.sourceAccount)
-            throw Error(`Source account mismatch.`);
-          const walletSigner = {
-            address: polkadotAccount.address,
-            signer: polkadotAccount.signer! as DotSigner,
-          };
-          const plan = await toEthereum.validateSend(
+          transfer = await handleSubstrateToEthereumTransfer({
             context,
-            walletSigner,
-            source.paraInfo.paraId,
-            data.beneficiary,
-            data.token,
+            source,
+            destination,
+            polkadotAccount,
+            data,
             amountInSmallestUnit,
-          );
-          console.log(plan);
-          if (plan.failure) {
-            track("Plan Failed", {
-              ...data,
-              errors: JSON.stringify(plan.failure.errors),
-            });
-            setBusyMessage("");
-            setError({
-              title: "Send Plan Failed",
-              description:
-                "Some preflight checks failed when planning the transfer.",
-              errors: plan.failure.errors.map((e) => ({
-                kind: "toEthereum",
-                ...e,
-              })),
-            });
-            return;
-          }
-
-          setBusyMessage(
-            "Waiting for transaction to be confirmed by wallet. After finalization transfers can take up to 4 hours.",
-          );
-          const result = await toEthereum.send(context, walletSigner, plan);
-          messageId = result.success?.messageId || "";
-          transfer = {
-            id: messageId,
-            status: history.TransferStatus.Pending,
-            info: {
-              amount: amountInSmallestUnit.toString(),
-              sourceAddress: data.sourceAccount,
-              beneficiaryAddress: data.beneficiary,
-              tokenAddress: data.token,
-              when: new Date(),
-            },
-            submitted: {
-              block_hash:
-                result.success?.sourceParachain?.blockHash ??
-                result.success?.assetHub.blockHash ??
-                "",
-              block_num:
-                result.success?.sourceParachain?.blockNumber ??
-                result.success?.assetHub.blockNumber ??
-                0,
-              block_timestamp: 0,
-              messageId: messageId,
-              account_id: data.source,
-              bridgeHubMessageId: "",
-              extrinsic_hash:
-                result.success?.sourceParachain?.txHash ??
-                result.success?.assetHub.txHash ??
-                "",
-              extrinsic_index:
-                result.success?.sourceParachain !== undefined
-                  ? result.success.sourceParachain.blockNumber.toString() +
-                    "-" +
-                    result.success.sourceParachain.txIndex.toString()
-                  : result.success?.assetHub !== undefined
-                    ? result.success?.assetHub?.blockNumber.toString() +
-                      "-" +
-                      result.success?.assetHub.txIndex.toString()
-                    : "unknown",
-
-              relayChain: {
-                block_hash: result.success?.relayChain.submittedAtHash ?? "",
-                block_num: 0,
-              },
-              success: true,
-            },
-          };
-          console.log(result);
+            setError,
+            setBusyMessage,
+          });
           break;
         }
         case "ethereum": {
-          if (destination.type !== "substrate")
-            throw Error(`Invalid form state: destination type mismatch.`);
-          if (destination.paraInfo === undefined)
-            throw Error(
-              `Invalid form state: destination does not have parachain id.`,
-            );
-          if (ethereumProvider === null)
-            throw Error(`Ethereum Wallet not connected.`);
-          if (ethereumAccount === null)
-            throw Error(`Wallet account not selected.`);
-          if (ethereumAccount !== data.sourceAccount)
-            throw Error(`Selected account does not match source data.`);
-          const signer = await ethereumProvider.getSigner();
-          if (signer.address.toLowerCase() !== data.sourceAccount.toLowerCase())
-            throw Error(`Source account mismatch.`);
-          const plan = await toPolkadot.validateSend(
+          transfer = await handleEthereumToSubstrateTransfer({
             context,
-            signer,
-            data.beneficiary,
-            data.token,
-            destination.paraInfo.paraId,
+            destination,
+            ethereumProvider,
+            ethereumAccount,
+            data,
             amountInSmallestUnit,
-            destination.paraInfo.destinationFeeDOT,
-            {
-              maxConsumers: destination.paraInfo.maxConsumers,
-              ignoreExistentialDeposit:
-                destination.paraInfo.skipExistentialDepositCheck,
-            },
-          );
-          console.log(plan);
-          if (plan.failure) {
-            track("Plan Failed", {
-              ...data,
-              errors: JSON.stringify(plan.failure.errors),
-            });
-            setBusyMessage("");
-            setError({
-              title: "Send Plan Failed",
-              description:
-                "Some preflight checks failed when planning the transfer.",
-              errors: plan.failure.errors.map((e) => ({
-                kind: "toPolkadot",
-                ...e,
-              })),
-            });
-            return;
-          }
-
-          setBusyMessage(
-            "Waiting for transaction to be confirmed by wallet. After finalization transfers can take up to 15-20 minutes.",
-          );
-          const result = await toPolkadot.send(context, signer, plan);
-
-          messageId = result.success?.messageId || "";
-          transfer = {
-            id: messageId,
-            status: history.TransferStatus.Pending,
-            info: {
-              amount: amountInSmallestUnit.toString(),
-              sourceAddress: data.sourceAccount,
-              beneficiaryAddress: data.beneficiary,
-              tokenAddress: data.token,
-              when: new Date(),
-              destinationParachain: destination.paraInfo.paraId,
-              destinationFee: destination.paraInfo.destinationFeeDOT.toString(),
-            },
-            submitted: {
-              blockHash: result.success?.ethereum.blockHash ?? "",
-              blockNumber: result.success?.ethereum.blockNumber ?? 0,
-              channelId: "",
-              messageId: messageId,
-              logIndex: 0,
-              transactionIndex: 0,
-              transactionHash: result.success?.ethereum.transactionHash ?? "",
-              nonce: 0,
-              parentBeaconSlot: 0,
-            },
-          };
-          console.log(result);
+            setError,
+            setBusyMessage,
+          });
           break;
         }
         default:
           throw Error(`Invalid form state: cannot infer source type.`);
       }
+
+      let messageId: string = transfer.id;
       track("Send Success", {
         ...data,
         messageId,
@@ -326,5 +176,207 @@ export function onSubmit({
         errors: [],
       });
     }
+  };
+}
+
+async function handleSubstrateToEthereumTransfer({
+  context,
+  source,
+  destination,
+  polkadotAccount,
+  data,
+  amountInSmallestUnit,
+  setError,
+  setBusyMessage,
+}: {
+  context: Context;
+  source: environment.TransferLocation;
+  destination: environment.TransferLocation;
+  polkadotAccount: WalletAccount | null;
+  amountInSmallestUnit: bigint;
+  data: FormData;
+  setError: Dispatch<SetStateAction<ErrorInfo | null>>;
+  setBusyMessage: Dispatch<SetStateAction<string>>;
+}): Promise<Transfer> {
+  if (destination.type !== "ethereum")
+    throw Error(`Invalid form state: destination type mismatch.`);
+  if (source.paraInfo === undefined)
+    throw Error(`Invalid form state: source does not have parachain info.`);
+  if (polkadotAccount === null) throw Error(`Polkadot Wallet not connected.`);
+  if (polkadotAccount.address !== data.sourceAccount)
+    throw Error(`Source account mismatch.`);
+
+  const walletSigner = {
+    address: polkadotAccount.address,
+    signer: polkadotAccount.signer! as DotSigner,
+  };
+  const plan = await toEthereum.validateSend(
+    context,
+    walletSigner,
+    source.paraInfo.paraId,
+    data.beneficiary,
+    data.token,
+    amountInSmallestUnit,
+  );
+  console.log(plan);
+  if (plan.failure) {
+    track("Plan Failed", {
+      ...data,
+      errors: JSON.stringify(plan.failure.errors),
+    });
+    setBusyMessage("");
+    setError({
+      title: "Send Plan Failed",
+      description: "Some preflight checks failed when planning the transfer.",
+      errors: plan.failure.errors.map((e) => ({
+        kind: "toEthereum",
+        ...e,
+      })),
+    });
+    throw Error("Plan validation failed");
+  }
+
+  setBusyMessage(
+    "Waiting for transaction to be confirmed by wallet. After finalization transfers can take up to 4 hours.",
+  );
+  const result = await toEthereum.send(context, walletSigner, plan);
+  const messageId = result.success?.messageId || "";
+  return {
+    id: messageId,
+    status: history.TransferStatus.Pending,
+    info: {
+      amount: amountInSmallestUnit.toString(),
+      sourceAddress: data.sourceAccount,
+      beneficiaryAddress: data.beneficiary,
+      tokenAddress: data.token,
+      when: new Date(),
+    },
+    submitted: {
+      block_hash:
+        result.success?.sourceParachain?.blockHash ??
+        result.success?.assetHub.blockHash ??
+        "",
+      block_num:
+        result.success?.sourceParachain?.blockNumber ??
+        result.success?.assetHub.blockNumber ??
+        0,
+      block_timestamp: 0,
+      messageId: messageId,
+      account_id: data.source,
+      bridgeHubMessageId: "",
+      extrinsic_hash:
+        result.success?.sourceParachain?.txHash ??
+        result.success?.assetHub.txHash ??
+        "",
+      extrinsic_index:
+        result.success?.sourceParachain !== undefined
+          ? result.success.sourceParachain.blockNumber.toString() +
+            "-" +
+            result.success.sourceParachain.txIndex.toString()
+          : result.success?.assetHub !== undefined
+            ? result.success?.assetHub?.blockNumber.toString() +
+              "-" +
+              result.success?.assetHub.txIndex.toString()
+            : "unknown",
+
+      relayChain: {
+        block_hash: result.success?.relayChain.submittedAtHash ?? "",
+        block_num: 0,
+      },
+      success: true,
+    },
+  };
+}
+async function handleEthereumToSubstrateTransfer({
+  context,
+  destination,
+  ethereumAccount,
+  ethereumProvider,
+  data,
+  amountInSmallestUnit,
+  setError,
+  setBusyMessage,
+}: {
+  context: Context;
+  destination: environment.TransferLocation;
+  ethereumAccount: string | null;
+  ethereumProvider: BrowserProvider | null;
+  amountInSmallestUnit: bigint;
+  data: FormData;
+  setError: Dispatch<SetStateAction<ErrorInfo | null>>;
+  setBusyMessage: Dispatch<SetStateAction<string>>;
+}): Promise<Transfer> {
+  if (destination.type !== "substrate")
+    throw Error(`Invalid form state: destination type mismatch.`);
+  if (destination.paraInfo === undefined)
+    throw Error(`Invalid form state: destination does not have parachain id.`);
+  if (ethereumProvider === null) throw Error(`Ethereum Wallet not connected.`);
+  if (ethereumAccount === null) throw Error(`Wallet account not selected.`);
+  if (ethereumAccount !== data.sourceAccount)
+    throw Error(`Selected account does not match source data.`);
+  const signer = await ethereumProvider.getSigner();
+  if (signer.address.toLowerCase() !== data.sourceAccount.toLowerCase())
+    throw Error(`Source account mismatch.`);
+  const plan = await toPolkadot.validateSend(
+    context,
+    signer,
+    data.beneficiary,
+    data.token,
+    destination.paraInfo.paraId,
+    amountInSmallestUnit,
+    destination.paraInfo.destinationFeeDOT,
+    {
+      maxConsumers: destination.paraInfo.maxConsumers,
+      ignoreExistentialDeposit:
+        destination.paraInfo.skipExistentialDepositCheck,
+    },
+  );
+  console.log(plan);
+  if (plan.failure) {
+    track("Plan Failed", {
+      ...data,
+      errors: JSON.stringify(plan.failure.errors),
+    });
+    setBusyMessage("");
+    setError({
+      title: "Send Plan Failed",
+      description: "Some preflight checks failed when planning the transfer.",
+      errors: plan.failure.errors.map((e) => ({
+        kind: "toPolkadot",
+        ...e,
+      })),
+    });
+    throw Error("Plan verification failed");
+  }
+
+  setBusyMessage(
+    "Waiting for transaction to be confirmed by wallet. After finalization transfers can take up to 15-20 minutes.",
+  );
+  const result = await toPolkadot.send(context, signer, plan);
+
+  const messageId = result.success?.messageId || "";
+  return {
+    id: messageId,
+    status: history.TransferStatus.Pending,
+    info: {
+      amount: amountInSmallestUnit.toString(),
+      sourceAddress: data.sourceAccount,
+      beneficiaryAddress: data.beneficiary,
+      tokenAddress: data.token,
+      when: new Date(),
+      destinationParachain: destination.paraInfo.paraId,
+      destinationFee: destination.paraInfo.destinationFeeDOT.toString(),
+    },
+    submitted: {
+      blockHash: result.success?.ethereum.blockHash ?? "",
+      blockNumber: result.success?.ethereum.blockNumber ?? 0,
+      channelId: "",
+      messageId: messageId,
+      logIndex: 0,
+      transactionIndex: 0,
+      transactionHash: result.success?.ethereum.transactionHash ?? "",
+      nonce: 0,
+      parentBeaconSlot: 0,
+    },
   };
 }
