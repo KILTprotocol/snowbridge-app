03aa56ddc11e8c25695f410df27ea89fc4a4f573
kilted-andres <andres@kilt.io>
2024-08-16 13:34:29 +0200

    fix: remove destinations as field from ParaConfig.Entry

--- utils/parachainConfig.ts
+++ utils/parachainConfig.ts
@@ -1,17 +1,7 @@
-interface Destination {
-  parents: number;
-  interior: {
-    X1: {
-      Parachain: number;
-    };
-  };
-}
-
 interface Entry {
   name: string;
   endpoint: string;
   pallet: string;
-  destination: Destination;
   parachainId: number;
 }
 
@@ -25,27 +15,11 @@ export const parachainConfig: ParaConfig = {
     endpoint: "wss://kilt.dotters.network",
     pallet: "assetSwitchPool1",
     parachainId: 2086,
-    destination: {
-      parents: 1,
-      interior: {
-        X1: {
-          Parachain: 2086,
-        },
-      },
-    },
   },
   Rilt: {
     name: "Rilt",
     endpoint: "wss://rilt.kilt.io",
     pallet: "assetSwitchPool1",
     parachainId: 4504,
-    destination: {
-      parents: 1,
-      interior: {
-        X1: {
-          Parachain: 4504,
-        },
-      },
-    },
   },
 };
